1c3b25a98e55e0f32e60735b996d2a2c75436f14
MAboelhadeed <moustafa.aboelhadeed92@gmail.com>
2025-04-24 23:10:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 
 import streamlit as st
 from dashboard import dashboard_ui, home_ui
-from dashboard.config import DASHBOARD_CONFIG
 
-st.set_page_config(
-    page_title="Elytrix",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="Elytrix", layout="wide", initial_sidebar_state="auto")
 
 with open("dashboard/assets/elytrix_logo.png", "rb") as logo_file:
     st.sidebar.image(logo_file.read(), width=80)
@@ -15,6 +10,11 @@ with open("dashboard/assets/elytrix_logo.png", "rb") as logo_file:
 st.sidebar.title("Elytrix Controls")
 theme = st.sidebar.radio("Select Theme", ["Dark", "Light"])
 page = st.sidebar.radio("Choose a tab", ["Home", "Dashboard"])
+
+if theme == "Dark":
+    st.markdown('<style>body { background-color: #0f1117; color: white; }</style>', unsafe_allow_html=True)
+else:
+    st.markdown('<style>body { background-color: #FFFFFF; color: black; }</style>', unsafe_allow_html=True)
 
 if page == "Home":
     home_ui.render_home()
